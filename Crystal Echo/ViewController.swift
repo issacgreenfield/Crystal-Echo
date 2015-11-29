1efb334d64e33ac97ef49abf8f3f5fcc1b217144
IssacGreenfield <issacgreenfield@gmail.com>
2015-11-29 11:54:59 -0800

    Starting to get the timer working in relation to the starting messages and proper resetting of the game array data.

--- Crystal Echo/ViewController.swift
+++ Crystal Echo/ViewController.swift
@@ -18,22 +18,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var button4: UIButton!
     @IBOutlet weak var button5: UIButton!
     
-    private var counter: Int = 1
+    private var shardPlaceCounter: Int = 1
     private var playState: Bool = false
     private var shardPattern: [Int] = []
-    private var startNewGame: Bool = true
+    private var wait: Bool = true
     private let newGame = GameBrain.init()
     private let play = MusicBrain.init()
-
+    
     @IBAction func buttonPressed(sender: UIButton)
     {
-        
+        self.view.userInteractionEnabled = false
         self.play.playShard(Int((sender.titleLabel?.text)!)!)
-        
-        
-//        self.view.userInteractionEnabled = false
-//        self.playButton(Int((sender.titleLabel?.text)!)!)
-//        self.view.userInteractionEnabled = true
+        if (Int((sender.titleLabel?.text)!)! != shardPlaceCounter)
+        {
+            self.gameOverMessage()
+            self.wait = true
+        }
+        else //you played the right shard
+        {
+            shardPlaceCounter++
+            self.view.userInteractionEnabled = true     //this is bad!!!!
+        }
     }
     
     private func playButton(buttonNumber: Int)
@@ -51,59 +56,93 @@ class ViewController: UIViewController {
         AudioServicesPlaySystemSound(mySound);
         
     }
-
+    
     private func setPattern(shardPattern: [Int])
     {
         self.shardPattern = shardPattern
     }
     
     func runTimedCode() {
-        
-        if self.startNewGame == true
+        if self.shardPlaceCounter == self.shardPattern.count
         {
+            self.wait = true
+        }else if self.shardPlaceCounter > self.shardPattern.count
+        {
+            //error handling here, Fix-it
+            print("shardPlaceCounter is out of bounds")
             self.startMessage()
-            self.startNewGame = false
-            self.playState = true
+        }
+        
+        if self.wait == true
+        {
+            //do nothing
         }else
         {
             if self.playState == true
             {
-                self.view.userInteractionEnabled = false
-                if self.counter <= self.shardPattern.count
+                //Just listen for user interactions
+            }else
+            {
+                if self.shardPlaceCounter < self.shardPattern.count
                 {
-                    playSound(self.shardPattern[self.counter - 1])
-                    self.counter++
+                    playSound(self.shardPattern[self.shardPlaceCounter - 1])
+                    self.shardPlaceCounter++
                 } else
                 {
-                    self.counter = 1
-                    self.playState = false
+                    self.shardPlaceCounter = 1
+                    self.playState = true
                     self.view.userInteractionEnabled = true
                 }
-            }else   //While the timer is not being used
-            {
-                //            print(self.counter)
             }
         }
     }
     
     private func startMessage()
     {
+
+        //handle reseting of data
+        self.shardPattern = []
+        self.newGame.resetPattern()
+        self.shardPattern = self.newGame.getDynamicPattern()
+
         let alertController = UIAlertController(title: "Welcome To", message:
             "Crystal Echo", preferredStyle: UIAlertControllerStyle.Alert)
-        alertController.addAction(UIAlertAction(title: "Start!", style: UIAlertActionStyle.Default,handler: nil))
+        alertController.addAction(UIAlertAction(title: "Start!", style: UIAlertActionStyle.Default,handler: {
+            (action: UIAlertAction!) in
+            let gameTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "runTimedCode", userInfo: nil, repeats: true)
+            }
+            ))
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+    private func gameOverMessage()
+    {
+        self.playState = false
+        let alertController = UIAlertController(title: "Darn!", message:
+            "That Ain't Right", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK, my bad...", style: UIAlertActionStyle.Default,handler: {
+            (action: UIAlertAction!) in
+            self.startMessage()
+            }
+            ))
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
+    
+    
     override func viewDidLoad() {
-
-        let gameTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "runTimedCode", userInfo: nil, repeats: true)
-       
+        
+//        let gameTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "runTimedCode", userInfo: nil, repeats: true)
+        
         self.view.userInteractionEnabled = false
-        
         super.viewDidLoad()
-        
-        }
-
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.startMessage()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
